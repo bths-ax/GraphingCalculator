df6755de6bafe184559c1b5bfa0f19efd0c71f46
bths-ax <ardenx@nycstudents.net>
2022-12-07 08:00:58 -0500

    wonder if we had to generate these ourselves
    oh well

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Polynomial"},{"p":"<Unnamed>","l":"PolynomialTester"}];updateSearchResults();
